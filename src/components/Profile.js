3b0867c529249acb9b1a42c6ab6216f03497f9b0
Joramba <jorkabozko@gmail.com>
2022-10-15 15:09:46 +0200

    feat: add skeleton of app

---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://img1.goodfon.com/original/1280x720/9/6f/minimalizm-gradient-background.jpg" alt="Back" />
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                New Post
+            </div>
+            <div>
+                Post 2
+            </div>
+            <div>
+                Post 1
+            </div>
+            Main content
+        </div>
+    )
+}
+
+export default Profile;
